492b39b57570cbf6153e84f0ca99197516108ff4
nat <nat.lownes@gmail.com>
2012-02-15 10:13:25 -0500

    init

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Fort Hill Company"
+maintainer_email "dev@forthillcompany.com"
+license          "Apache 2.0"
+description      "Installs/Configures xvfb-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
